f4b3b263e15b29febfb4d525c2136bb4e78ba12c
libeanim <libeanim@users.noreply.github.com>
2020-08-30 13:13:17 +0000

    Add loss callback to TRAIN function

--- utils/training.py
+++ utils/training.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-def TRAIN(env, model, name, learning_rate=1e-3, loss='mse', initialise=True):
+def TRAIN(env, model, name, learning_rate=1e-3, loss='mse', initialise=True, learn_friction=False):
     """
     Train a model given an environment and the learning parameters.
 
@@ -15,15 +15,20 @@ def TRAIN(env, model, name, learning_rate=1e-3, loss='mse', initialise=True):
                         If False model will not be reinitialised so that training can continue.
     """
     if initialise:
-        m = model(env.dt, env.horizon, name=name, dim_state=env.X.shape[1], learn_inertia=False, activation='tanh')
+        m = model(env.dt, env.horizon, name=name, dim_state=env.X.shape[1],
+                  learn_inertia=False, learn_friction=learn_friction, activation='tanh')
         optimizer = tf.keras.optimizers.Adam(learning_rate)
         m.compile(optimizer, loss=loss, run_eagerly=False)
     else:
         m = model
+    loss = []
+    log = tf.keras.callbacks.LambdaCallback(
+        on_epoch_end=lambda epoch, log: loss.append([epoch, log['loss']]))
     m.fit(
         tf.convert_to_tensor(env.X.reshape(env.X.shape[0], 1, env.X.shape[1]), np.float32),
         tf.convert_to_tensor(env.y.reshape(env.y.shape[0], env.horizon, env.X.shape[1]), np.float32),
-        epochs=env.epochs, shuffle=True)
+        epochs=env.epochs, shuffle=True, callbacks=[log])
+    m.loss_data = np.array(loss)
     return m
 
 def PREDICT(env, model):
