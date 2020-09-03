7cdb1e2f5068f642afb45f9fad83a6c3ce10d755
libeanim <libeanim@users.noreply.github.com>
2020-09-03 11:55:54 +0000

    Save images by default in main.py

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 # %% Init
+import os
 from experiments import pendulum, pendulum_friction, bouncing_ball, newton_cradle
+
+# Create path for images
+try:
+    if not os.path.exists('images'):
+        os.mkdir('images')
+except:
+    print('Unable to create image folder.')
 # %% Pendulum
 pendulum.run(train_vin=True)
 
-pendulum.plot_trajectory()
-pendulum.plot_energy()
-pendulum.plot_potential()
+pendulum.plot_trajectory(savefig=True)
+pendulum.plot_energy(savefig=True)
+pendulum.plot_potential(savefig=True)
 
 # %% Pendulum friction
 pendulum_friction.run(train_vin=True)
 
-pendulum_friction.plot_trajectory()
-pendulum_friction.plot_energy()
-pendulum_friction.plot_potential()
+pendulum_friction.plot_trajectory(savefig=True)
+pendulum_friction.plot_energy(savefig=True)
+pendulum_friction.plot_potential(savefig=True)
 
 # %% Bouncing Ball
 bouncing_ball.run()
 
-bouncing_ball.plot_trajectory()
-bouncing_ball.plot_potential()
+bouncing_ball.plot_trajectory(savefig=True)
+bouncing_ball.plot_potential(savefig=True)
 
 # %% Newton Cradle
 newton_cradle.run()
 
-newton_cradle.plot_trajectory()
-newton_cradle.plot_potential()
+newton_cradle.plot_trajectory(savefig=True)
+newton_cradle.plot_potential(savefig=True)
