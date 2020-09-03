ee4cd03dc5e0f50d743d1752b18606bda15beae1
libeanim <libeanim@users.noreply.github.com>
2020-09-03 13:58:37 +0200

    Add testcases for Newton Cradle + Bouncing Ball

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,10 +1,9 @@
 #%%
 import sys
 sys.path.append('../')
-from environments import Pendulum
+from environments import Pendulum, BouncingBall, NewtonCradle
 
-# Simulate diffrent pendulum scenarios to determine the correctness of the system
-#%%
+#%% Pendulum
 # Test if step count is correct
 test_steps = 400
 test_horizon=10
@@ -25,3 +24,47 @@ assert e2.y.shape[0] == test_steps - test_horizon - 1
 assert e2.y.shape[1] == 2 * test_horizon
 
 print('Successfully finished.')
+#%% Bouncing Ball
+# Test if step count is correct
+test_steps = 400
+test_horizon=10
+e1 = BouncingBall(steps=test_steps-1, horizon=test_horizon, dt=0.01, epochs=3000, SIGMA=0.1)
+e1.generate()
+assert e1.trajectory.shape[0] == test_steps
+assert e1.trajectory.shape[1] == 2
+assert e1.y.shape[0] == test_steps - test_horizon - 1
+assert e1.X.shape[0] == test_steps - test_horizon - 1
+assert e1.X.shape[1] == 2
+assert e1.y.shape[1] == 2 * test_horizon
+
+# Test if horizon works properly
+test_horizon=5
+e2 = BouncingBall(steps=test_steps-1, horizon=test_horizon, dt=0.01, epochs=3000, SIGMA=0.1)
+e2.generate()
+assert e2.y.shape[0] == test_steps - test_horizon - 1
+assert e2.y.shape[1] == 2 * test_horizon
+
+print('Successfully finished.')
+
+#%% Newton Cradle
+# Test if step count is correct
+test_steps = 400
+test_horizon=10
+e1 = NewtonCradle(steps=test_steps-1, horizon=test_horizon, dt=0.01, epochs=3000, SIGMA=0.1)
+e1.generate()
+assert e1.trajectory.shape[0] == test_steps
+assert e1.trajectory.shape[1] == 4
+assert e1.y.shape[0] == test_steps - test_horizon - 1
+assert e1.X.shape[0] == test_steps - test_horizon - 1
+assert e1.X.shape[1] == 4
+assert e1.y.shape[1] == 4 * test_horizon
+
+# Test if horizon works properly
+test_horizon=5
+e2 = NewtonCradle(steps=test_steps-1, horizon=test_horizon, dt=0.01, epochs=3000, SIGMA=0.1)
+e2.generate()
+assert e2.y.shape[0] == test_steps - test_horizon - 1
+assert e2.y.shape[1] == 4 * test_horizon
+
+print('Successfully finished.')
+# %%
